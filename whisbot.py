aeaf3462d7520f85bf5eb6658dff704319ddf1db
kawzaknobom <142733725+kawzaknobom@users.noreply.github.com>
2026-01-11 22:19:41 +0200

    Update whisbot.py

--- whisbot.py
+++ whisbot.py
@@ -1,9 +1,19 @@
+import nest_asyncio
+nest_asyncio.apply()
+
+#########################################################
+
+Bot_Token = 'Put UR Telegram Token Here'
+
+########################################################
+
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , CallbackQuery , ForceReply,Message
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram import idle
 from textwrap import wrap
 import os,time,shutil,asyncio
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
 from faster_whisper import WhisperModel
 
 Api_Id = 15952578
@@ -19,43 +29,40 @@ async def Mp3_Conv(File):
 
 async def whisper_transcribe(media_file):
   TxtFile = ('.' if media_file[0]=='.' else '' ) + media_file.split('.')[1 if media_file[0]=='.' else 0 ] + '_WTranscribed.txt'
-  media_file = await Mp3_Conv(media_file) 
-  model = await WhisperModel("large-v3-turbo", device="cpu", compute_type="int8")
-  segments, info = await model.transcribe(media_file, beam_size=5, vad_filter=True)
+  media_file = await Mp3_Conv(media_file)
+  model = WhisperModel("large-v3-turbo", device="cuda", compute_type="int8")
+  segments, info = model.transcribe(media_file, beam_size=5, vad_filter=True)
   for segment in segments:
-    open(TxtFile,'a').write(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}\n")
+    open(TxtFile,'a').write(f"{segment.text}")
   return TxtFile
-    
+
 def Pyrogram_Client(Bot_Token):
   Bot_Identifier = Bot_Token.split(':')[0]
-  Session_file = Bot_Identifier+'_session_prm_bot'
-  bot = Client(Session_file,api_id=Api_Id,api_hash=Api_Hash,bot_token=Bot_Token)
+  Session_file = Bot_Identifier+'_955hyh95|session_prm_bot'
+  bot = Client(Session_file,api_id=Api_Id,api_hash=Api_Hash,bot_token=Bot_Token,in_memory=True)
   return bot,Bot_Identifier
-  
-Bot_Token = '8516897868:AAG8tyDbCdDYmcQHyBgnxf_fI5nwDUulcMY'
+
 bot,Bot_Identifier = Pyrogram_Client(Bot_Token)
 dl_path = f'./downloads_{Bot_Identifier}/'
 
-@bot.on_message(filters.private & filters.incoming & (filters.audio | filters.voice | filters.video | filters.document ))
+@bot.on_message(filters.private & filters.incoming & (filters.audio | filters.voice | filters.video ))
 async def _telegram_file(client, message):
+  reply_msg = await message.reply('جار التفريغ')
   file = await message.download(file_name=dl_path)
   Txt_File = await whisper_transcribe(file)
   await message.reply_document(Txt_File)
-  shutil.rmtree(dl_path)
+  os.remove(Txt_File)
+  await reply_msg.edit_text('تم التفريغ ✅')
 
-async def run_my_bot():
-    # This manually awaits the start coroutine that was causing the warning
-    await bot.start()
-    print("Successfully started! Send a message to your bot to test.")
-    
-    # This keeps the bot running until you manually stop the cell
-    await idle()
-    
-    # Gracefully shuts down
-    await bot.stop()
 
-# Execution
-try:
-    asyncio.get_event_loop().create_task(run_my_bot())
-except Exception as e:
-    print(f"Failed to run: {e}")
+async def main():
+    if not os.path.exists(dl_path): os.makedirs(dl_path)
+    try:
+        await bot.start()
+        print("✅ Whisper Bot is ONLINE!")
+        await idle()
+    finally:
+        if bot.is_connected:
+            await bot.stop()
+
+await main()
